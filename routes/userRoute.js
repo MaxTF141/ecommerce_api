ce702e1a4fbe19e99e616dbc1859f2db25a4d077
your email address <your email address>
2023-02-16 23:20:49 +0200

    origin

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -23,8 +23,8 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res,)=>{
     try{
-        const {id} = req.body;
-         con.query(`SELECT * FROM users WHERE id = ${id} `,[id], (err, result)=>{
+        const {id} = req.params;
+         con.query('SELECT * FROM users WHERE id = ? ', [id], (err, result)=>{
             if(err) throw err;
             res.send(result);
          })
@@ -48,6 +48,34 @@ router.post('/', function(req, res,){
         res.status(400).send(error)
     }
 });
+
+router.put('/:id', (req, res)=> {
+    try{
+        const {id} = req.params;
+        const {email, password, full_name, billing_address, default_shipping_address, country, phone} = req.body;
+
+        con.query('UPDATE users SET email = ?, password = ?, full_name = ?, billing_address = ?, default_shipping_address = ?, country = ?, phone = ? WHERE id = ?', [email, password, full_name, billing_address, default_shipping_address, country, phone ,id], (err, result)=>{
+            if(err) throw err;
+            res.send(result);
+        })
+    } catch(error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+
+router.delete('/:id', (req, res)=> {
+    try {
+        const{id} = req.params;
+        con.query('DELETE FROM users WHERE id = ?', [id], (err, results)=> {
+            if(err) throw err;
+            res.send(results);
+        })
+    } catch (error){
+        console.log(error);
+        res.status(400).send(error);
+    }
+})
 
 
 module.exports = router;
